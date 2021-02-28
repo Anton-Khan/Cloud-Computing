9edb57847c080dcde77b065d830128f994fa895d
Anton <31567808+Anton-Khan@users.noreply.github.com>
2021-03-01 00:26:43 +0300

    Pure Function

--- Practice_1/Task1.js
+++ Practice_1/Task1.js
@@ -2,13 +2,11 @@ function Sum(arr, index=0, sum=0){
     if (index >= arr.length ) {
         return sum;
     }else{
-        sum += arr[index];
-        return Sum(arr, index+1, sum);
+        return Sum(arr, index+1, sum + arr[index]);
     }
 }
 
 let arr = [1,2,3,4,5,6,7,8];
-
 let result = Sum(arr);
 
 console.log(result);
