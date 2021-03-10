8771bd5e1c56b085229ca76fe8a8b9708828cee3
Anton <31567808+Anton-Khan@users.noreply.github.com>
2021-03-10 13:37:58 +0300

    Pure Function

--- Practice_1/Task2.js
+++ Practice_1/Task2.js
@@ -1,12 +1,13 @@
+function ConvertStringsToLengthItemArray(array){
+    return array.map((item)=>item.length);
+}
 //находит самую длинную строку вывод:[строка, её длинна]
-function FindStringWithMaxLength(array){
-    let lengthItemArray = array.map((item)=>item.length);
-    let maxLength = Math.max.apply(null, lengthItemArray);
-    return [array[lengthItemArray.indexOf(maxLength)] , maxLength];
+function FindStringWithMaxLength(array, lengthArray){
+    return [array[lengthArray.indexOf(Math.max.apply(null, lengthArray))], Math.max.apply(null, lengthArray)];
 }
 //находит самыю длинныю строки в сгруппированном массиве([ ["a", "aaa"], ["bbb", "b"], ... ]) вывод:[[строка, её длинна], [строка, её длинна]]
 function FindAllStringsWithMaxLength(groupedArray){
-    return groupedArray.reduce((result, item)=> [...result, FindStringWithMaxLength(item)], []);
+    return groupedArray.reduce((result, item)=> [...result, FindStringWithMaxLength(item, ConvertStringsToLengthItemArray(item))], []);
 }
 //возвращает новый сгруппированый массив вида [ ["a", "aaa"], ["bbb", "b"], ... ]
 function TransformToGroupedArray(array, uniqueLetters){
@@ -18,13 +19,9 @@ function FindUniqueLetters(array){
     .filter((x, index, self)=>self.indexOf(x) === index);
 }
 //ввод: ["aaa", "b", "b", "aa"], вывод: [ ["aaa", 3], ["b", 1] ]
-function FindUniqueStringsWithMaxLength(arr){
-    let uniqueLetters = FindUniqueLetters(arr);
-    let groupedArray = TransformToGroupedArray(arr, uniqueLetters);
-    let result = FindAllStringsWithMaxLength(groupedArray);
-    return result;
+function FindUniqueStringsWithMaxLength(array){
+    return FindAllStringsWithMaxLength(TransformToGroupedArray(array, FindUniqueLetters(array)));
 }
-
 
 let arr = ["aaaaaaa", "bbb", "b", "b", "aaa", "ccccccccc", "aaaaaa", "qqqqqqqqqqqqqqqqq", "q"];
 let result = FindUniqueStringsWithMaxLength(arr);
